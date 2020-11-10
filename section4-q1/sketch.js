98db1550bd9b98372f7f6221dcf4d09b72de0845
tanyhiro <1970867h@stu.kobe-u.ac.jp>
2020-11-10 14:29:33 +0900

    section4-1q 必須　完成

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -11,6 +11,6 @@ function setup(){
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  size = constrain(count,50,200)// BLANK[1]
   ellipse(width / 2, height / 2, size);
 }
