ce193da55abe363f2978ff0d70290bc03bf07fa9
tanyhiro <1970867h@stu.kobe-u.ac.jp>
2020-11-17 10:49:46 +0900

    4-q2 finished

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,17 +1,27 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy,angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 6;
+  vy = 6;
+  angle = 0
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(27, 28, 128);
   // BLANK[2] (hint: 作った star 関数を使います)
+  fill(218,218,0);
+  noStroke();
+   x += vx
+   y += vy
+  angle += 0.01;
+translate(x,y);
+  rotate(angle);
+  star(0,0,width/100,angle);
+
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -20,10 +30,10 @@ function draw(){
   if(y < 0){ y = height; }
 }
 
-function star(cx, cy, r, angle){
+function star(cx, cy, r,angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
